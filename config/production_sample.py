e866afbf026b2d2da28cca71f06486c2793af33e
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-02-27 15:37:12 +0300

    changed production-sample config

--- config/production_sample.py
+++ config/production_sample.py
@@ -6,13 +6,10 @@ class ProductionConfig(Config):
     # App config
     SECRET_KEY = "\xb5\xb3}#\xb7A\xcac\x9d0\xb6\x0f\x80z\x97\x00\x1e\xc0\xb8+\xe9)\xf0}"
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 7
-    SESSION_COOKIE_NAME = 'app_session'
+    SESSION_COOKIE_NAME = 'illumiart_session'
 
     # Site domain
-    SITE_DOMAIN = "http://www.app.com"
+    SITE_DOMAIN = "illumiart.by"
 
     # Db config
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/app"
-
-    # Sentry
-    SENTRY_DSN = ''
+    SQLALCHEMY_DATABASE_URI = "postgresql://illumiart@localhost/illumiart"
